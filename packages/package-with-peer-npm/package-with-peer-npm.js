f10c9311f34af6526bb21886abd7dc2c5c54c94e
Tom Coleman <tom@thesnail.org>
2016-03-03 14:35:34 +1100

    repro

---
+++ packages/package-with-peer-npm/package-with-peer-npm.js
@@ -0,0 +1,4 @@
+import { checkNpmVersions } from 'meteor/tmeasday:check-npm-versions';
+checkNpmVersions({moment: '2.11.x'});
+
+export const foo = 42;
---
+++ tests.js
@@ -0,0 +1,1 @@
+import 'meteor/package-with-peer-npm';
